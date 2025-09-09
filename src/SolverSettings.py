69adf801570d05f2ab0b0d1a2a97cfd5b22a90ad
ndria00 <ctrndr00e01i872o@studenti.unical.it>
2025-09-09 09:31:16 +0200

    updated SolverSettings

---
+++ src/SolverSettings.py
@@ -0,0 +1,16 @@
+
+class SolverSettings:
+
+    n_models : int
+    debug : bool
+    constraint_print : bool
+    relaxed_solving: bool
+    enumeration : bool
+
+
+    def __init__(self, n_models, debug, constraint_print, relaxed_solving):
+        self.n_models = n_models
+        self.debug = debug
+        self.relaxed_solving = relaxed_solving
+        self.constraint_print = constraint_print
+        self.enumeration = True if n_models == 0 else False
